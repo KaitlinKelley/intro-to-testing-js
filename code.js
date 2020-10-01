32cb73b2cc551c52d4cc1f694f599d0fdc20e9d9
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2020-10-01 14:52:42 -0500

    refactored sayHello, got rid of if/else

--- code.js
+++ code.js
@@ -3,12 +3,18 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello(input){
-    if(input === "Alex"){
-        return "Hello, Alex!";
-    } else if(input === "Pat"){
-        return "Hello, Pat!";
-    } else return "Hello, Jane!";
+// function sayHello(input){
+//     if(input === "Alex"){
+//         return "Hello, Alex!";
+//     } else if(input === "Pat"){
+//         return "Hello, Pat!";
+//     } else return "Hello, Jane!";
+//
+// }
 
+//------Refactoring....
+
+function sayHello(input){
+    return "Hello, " + input + "!";
 }
 
